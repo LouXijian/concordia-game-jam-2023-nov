a51279ff558e480f44e8a67f422e32d3b8be70d0
xijianlou1 <xijian.lou@unity3d.com>
2023-11-04 12:49:35 -0400

    feature: update the cushion time, so the player doesn't need to move on the exact pulse.

--- concordia-game-jam-2023-nov/Assets/Script/Slumber/Slumber.cs
+++ concordia-game-jam-2023-nov/Assets/Script/Slumber/Slumber.cs
@@ -6,20 +6,30 @@ public class Slumber : MonoBehaviour
 {
     public int SlumberLevel;
     public int SlumberLevelMax = 100;
-    public ISound Sound;
+    public HeartbeatSound Sound;
     public int SlumberLevelDamage = 5;
     public PlayerController Player;
     
     void Start()
     {
         Player.OnPlayerMove += WakeningMonster;
+        SlumberLevel = SlumberLevelMax;
     }
     
     public void WakeningMonster()
     {
         if (!Sound.DetectPulse())
         {
+            Debug.Log("Your step is heard!");
             SlumberLevel -= SlumberLevelDamage;
+            if (SlumberLevel <= 60)
+            {
+                Debug.Log("The monster is waking up!");
+            }
+            if (SlumberLevel <= 0)
+            {
+                Debug.Log("Ouch! You woke up the monster!");
+            }
         }
     }
 }
