62ae2f031a1046cd0fbd5fe6d143b879d9299904
yanouli <yanou.li@mail.mcgill.ca>
2023-11-04 16:18:04 -0400

    feature: heart beat sound system.

--- concordia-game-jam-2023-nov/Assets/Script/Slumber/Slumber.cs
+++ concordia-game-jam-2023-nov/Assets/Script/Slumber/Slumber.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+/*using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -30,6 +30,57 @@ public class Slumber : MonoBehaviour
             {
                 Debug.Log("Ouch! You woke up the monster!");
             }
+        }
+    }
+}
+*/
+using UnityEngine;
+using System.Collections;
+
+public class Slumber : MonoBehaviour
+{
+    public int SlumberLevel;
+    public int SlumberLevelMax = 100;
+    public HeartbeatSound Sound;
+    public int SlumberLevelDamage = 5;
+    public PlayerController Player;
+    
+    private float lastPulseTime = 0f;
+    private float pulseInterval = 2f; // You should set this to the interval of your heartbeat sounds.
+
+    void Start()
+    {
+        Player.OnPlayerMove += WakeningMonster;
+        SlumberLevel = SlumberLevelMax;
+        // You might need to initialize or get the pulse interval from the Sound instance.
+        // pulseInterval = Sound.GetPulseInterval();
+        lastPulseTime = Time.time; // Assuming the pulse starts at the beginning.
+    }
+    
+    public void WakeningMonster()
+    {
+        // Check if the current time is within the interval after the last pulse
+        if (Time.time >= lastPulseTime + pulseInterval && !Sound.DetectPulse())
+        {
+            Debug.Log("Your step is heard!");
+            SlumberLevel -= SlumberLevelDamage;
+
+            // Reset the last pulse time
+            lastPulseTime = Time.time;
+
+            CheckSlumberStatus();
+        }
+    }
+
+    private void CheckSlumberStatus()
+    {
+        if (SlumberLevel <= 60)
+        {
+            Debug.Log("The monster is waking up!");
+        }
+        if (SlumberLevel <= 0)
+        {
+            Debug.Log("Ouch! You woke up the monster!");
         }
     }
 }
--- concordia-game-jam-2023-nov/Assets/Script/Sound/HeartbeatSound.cs
+++ concordia-game-jam-2023-nov/Assets/Script/Sound/HeartbeatSound.cs
@@ -1,46 +1,71 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
 using UnityEngine;
 using Random = UnityEngine.Random;
 
 public class HeartbeatSound : MonoBehaviour
 {
+    public AudioClip HeartbeatClip; // Assign this in the inspector
+    private AudioSource audioSource; // AudioSource component to play the sound
+
     [HideInInspector]
     public List<float> PulseList;
-    public float PulseInterfal = 0.5f;
+    public float PulseInterval = 2f; // Please note the typo correction: PulseInterfal -> PulseInterval
     public float MusicLength = 100f;
-    public float RandomLevel = 0.5f;
+    public float RandomLevel = 1f;
     private float m_HeartBeatStartTime;
     private float m_CushionTime = 0.1f;
+    public PlayerController Player;
 
     void Start()
     {
-        for (int i = 0; i * PulseInterfal < MusicLength; i++)
+        audioSource = gameObject.AddComponent<AudioSource>();
+        audioSource.clip = HeartbeatClip;
+        audioSource.playOnAwake = false;
+
+        PulseList = new List<float>();
+        PulseList.Add(0); // First pulse at time 0
+        for (int i = 1; i * PulseInterval < MusicLength; i++)
         {
-            PulseList.Add(i * PulseInterfal);
+            PulseList.Add(i * PulseInterval);
             if (Random.Range(0f, 1f) > RandomLevel)
             {
                 PulseList.Add(Random.Range(0f, MusicLength));
             }
         }
+        PulseList.Sort(); // Ensure the list is sorted after adding random pulses
+
         m_HeartBeatStartTime = Time.time;
+
+        Player.OnPlayerMove += PlayStepSound;
+    }
+
+    void Update()
+    {
+        if(DetectPulse())
+        {
+            audioSource.Play();
+        }
+    }
+
+    void PlayStepSound()
+    {
+        Debug.Log("Step sound is played.");
     }
 
     public bool DetectPulse()
     {
-        for (int i = 1; i < PulseList.Count; i++)
+        float timeSinceStart = Time.time - m_HeartBeatStartTime;
+        // Check if the time since start is within any pulse window
+        for (int i = 0; i < PulseList.Count; i++)
         {
-            float currentElement = PulseList[i];
-            float currentDifference = Math.Abs(Time.time - m_HeartBeatStartTime - currentElement);
-
-            if (currentDifference < m_CushionTime)
+            float pulseTime = PulseList[i];
+            if (timeSinceStart >= pulseTime - m_CushionTime && timeSinceStart <= pulseTime + m_CushionTime)
             {
                 return true;
             }
         }
-
         return false;
     }
 }
