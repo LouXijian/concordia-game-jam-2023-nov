12e15b1204519aab2d3ef280d5ca10b3a6d1d99b
xijianlou1 <xijian.lou@unity3d.com>
2023-11-04 11:03:24 -0400

    feature: heartbeat system.

--- concordia-game-jam-2023-nov/Assets/Script/Sound/HeartbeatSound.cs
+++ concordia-game-jam-2023-nov/Assets/Script/Sound/HeartbeatSound.cs
@@ -1,18 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class HeartBeat : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public List<float> PulseList;
+    public float PulseInterfal = 0.5f;
+    public float MusicLength = 100f;
+    public float RandomLevel = 0.5f;
+    private readonly float m_HeartBeatStartTime = Time.time;
+
+    public HeartBeat()
     {
-        
+        for (int i = 0; i * PulseInterfal < MusicLength; i++)
+        {
+            PulseList.Add(i * PulseInterfal);
+            if (Random.Range(0f, 1f) > RandomLevel)
+            {
+                PulseList.Add(Random.Range(0f, MusicLength));
+            }
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public bool DetectPulse()
     {
-        
+        var currentTime = Time.time;
+        return PulseList.Any(pulse => pulse == currentTime - m_HeartBeatStartTime);
     }
 }
