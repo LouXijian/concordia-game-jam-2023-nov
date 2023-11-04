28f23e3e0590f75c1efe47c3816bab58318d25b4
xijianlou1 <xijian.lou@unity3d.com>
2023-11-04 18:16:01 -0400

    fix: clean up Slumber code.

--- concordia-game-jam-2023-nov/Assets/Script/Slumber/Slumber.cs
+++ concordia-game-jam-2023-nov/Assets/Script/Slumber/Slumber.cs
@@ -9,42 +9,29 @@ public class Slumber : MonoBehaviour
     public int SlumberLevelDamage = 5;
     public PlayerController Player;
     
-    private float lastPulseTime = 0f;
     private float pulseInterval = 2f; // You should set this to the interval of your heartbeat sounds.
 
     void Start()
     {
         Player.OnPlayerMove += WakeningMonster;
         SlumberLevel = SlumberLevelMax;
-        // You might need to initialize or get the pulse interval from the Sound instance.
-        // pulseInterval = Sound.GetPulseInterval();
-        lastPulseTime = Time.time; // Assuming the pulse starts at the beginning.
     }
-    
+
     public void WakeningMonster()
     {
-        // Check if the current time is within the interval after the last pulse
-        if (Time.time >= lastPulseTime + pulseInterval && !Sound.DetectPulse())
+        if (!Sound.DetectPulse())
         {
             Debug.Log("Your step is heard!");
             SlumberLevel -= SlumberLevelDamage;
+            if (SlumberLevel <= 60)
+            {
+                Debug.Log("The monster is waking up!");
+            }
 
-            // Reset the last pulse time
-            lastPulseTime = Time.time;
-
-            CheckSlumberStatus();
-        }
-    }
-
-    private void CheckSlumberStatus()
-    {
-        if (SlumberLevel <= 60)
-        {
-            Debug.Log("The monster is waking up!");
-        }
-        if (SlumberLevel <= 0)
-        {
-            Debug.Log("Ouch! You woke up the monster!");
+            if (SlumberLevel <= 0)
+            {
+                Debug.Log("Ouch! You woke up the monster!");
+            }
         }
     }
 }
