54c0eb951d7864d028523bddc8e26de8da08a8f5
yanouli <yanou.li@mail.mcgill.ca>
2023-11-05 18:31:03 -0500

    added more sound effect

--- concordia-game-jam-2023-nov/Assets/Script/UI/GameOverController.cs
+++ concordia-game-jam-2023-nov/Assets/Script/UI/GameOverController.cs
@@ -5,6 +5,7 @@ public class GameOverController : MonoBehaviour
 {
     public GameObject gameOverPanel; // Assign a UI Panel from the scene to this
     public Text gameOverText; // Assign a UI Text inside the panel to display messages
+    public AudioSource monsterRoar;
 
     public void ShowGameOver(bool win)
     {
@@ -13,5 +14,18 @@ public class GameOverController : MonoBehaviour
             gameOverText.text = "Fortune is collected in its slumber. \nYou win!";
         else
             gameOverText.text = "Game Over. \nThe monster wakes up.";
+            PlayMonsterRoar();
     }
+
+       private void PlayMonsterRoar()
+    {
+        if (monsterRoar != null)
+        {
+            monsterRoar.Play();
+        }
+    }
+
+
+
+
 }
